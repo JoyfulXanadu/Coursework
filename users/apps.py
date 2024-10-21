16fa889bee7b0e9e9ed2974c17fbcbacaead684e
sienc <serenecodedrive247@gmail.com>
2024-10-21 10:14:19 +0500

    Commit

--- users/apps.py
+++ users/apps.py
@@ -1,6 +1,14 @@
+import os
 from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+    def ready(self):
+        import users.signals
+
+        if os.environ.get('RUN_MAIN'):
+            from users.services import start_scheduler
+            start_scheduler()
